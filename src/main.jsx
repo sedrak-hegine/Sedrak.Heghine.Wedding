17dfef983b08ce23069d26584e83af3284f8158a
sedrak-hegine <sedrak.g.khahcatryan@gmail.com>
2026-02-12 14:30:27 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // This is the connection!
+import './index.css' // <--- MAKE SURE THIS LINE EXISTS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
