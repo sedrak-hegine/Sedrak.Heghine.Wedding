3ef48604557e6c7f0bdf89db2adb68d0fe872bef
sedrak-hegine <sedrak.g.khahcatryan@gmail.com>
2026-02-12 14:03:34 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Change this to your repository name
-const repoName = '/sedrak-heghine-wedding/';
+// EXACT repository name with slashes
+const repoName = '/Sedrak.Heghine.Wedding/';
 
 export default defineConfig({
   plugins: [react()],
